8b0269e73cb980e07ac31996328f8ae590175465
stefan-hoeck <hock@zhaw.ch>
2026-08-09 06:58:17 +0200

    [ lint ] shut up, linter

--- makedocs.sh
+++ makedocs.sh
@@ -6,7 +6,7 @@ pack typecheck tpl
 cp -v doc/main.typ pdf/main.typ
 cp -v doc/template.typ pdf/template.typ
 
-katla-typst src/TPL/BoolExp/Term.typ build/ttc/*/TPL/BoolExp/Term.ttm > pdf/bool_term.typ
-katla-typst src/TPL/BoolExp/Parser.typ build/ttc/*/TPL/BoolExp/Parser.ttm > pdf/bool_parser.typ
+katla-typst src/TPL/BoolExp/Term.typ build/ttc/*/TPL/BoolExp/Term.ttm >pdf/bool_term.typ
+katla-typst src/TPL/BoolExp/Parser.typ build/ttc/*/TPL/BoolExp/Parser.ttm >pdf/bool_parser.typ
 
 typst c pdf/main.typ pdf/doc.pdf
