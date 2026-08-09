504b795bd70b621736d34e2eda95492c42f30505
Stefan Höck <hock@zhaw.ch>
2026-08-09 05:22:00 +0000

    [ doc ] use idris2 --ttc-version when building docs (#101)
    * [ doc ] use idris2 --ttc-version when building docs
    
    * [ lint ] shut up, linter

--- makedocs.sh
+++ makedocs.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+TTC=$(idris2 --ttc-version)
+BUILD="build/ttc/$TTC"
+
 pack install-app katla-typst
 pack typecheck tpl
 
 cp -v doc/main.typ pdf/main.typ
 cp -v doc/template.typ pdf/template.typ
 
-katla-typst src/TPL/BoolExp/Term.typ build/ttc/*/TPL/BoolExp/Term.ttm >pdf/bool_term.typ
-katla-typst src/TPL/BoolExp/Parser.typ build/ttc/*/TPL/BoolExp/Parser.ttm >pdf/bool_parser.typ
+katla-typst src/TPL/BoolExp/Term.typ "$BUILD"/TPL/BoolExp/Term.ttm >pdf/bool_term.typ
+katla-typst src/TPL/BoolExp/Parser.typ "$BUILD"/TPL/BoolExp/Parser.ttm >pdf/bool_parser.typ
 
 typst c pdf/main.typ pdf/doc.pdf
